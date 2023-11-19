8938bda440aa0ab77746e714a7bb260f8f86212e
Connor Young <connor-young@outlook.com>
2023-11-19 11:28:21 -0800

    - removed printf on thread_unblock; seemed to cause side effects to priority-condvar

--- Assignment_1/src/threads/thread.c
+++ Assignment_1/src/threads/thread.c
@@ -276,7 +276,7 @@ thread_unblock (struct thread *t)
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
   //list_push_back (&ready_list, &t->elem);
-  printf("Unblocking thread %s\n", t->name);
+  //printf("Unblocking thread %s\n", t->name);
   list_insert_ordered(&ready_list, &t->elem, less_priority, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
